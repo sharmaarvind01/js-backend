1d6adc11894961bf597e9e874cad0b6da3fd0909
Arvind Sharma <sharma.arvind@neosoftmail.com>
2024-02-13 17:19:54 +0530

    Mongo db connection added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-  
+// require('dotenv').config({path: "./env"})
+
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+import express from "express";
+const app = express();
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB();
+
+/*(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERR: ",error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listning on http://localhost:${process.env.PORT}`)
+        })
+    }catch (error){
+        console.error("ERROR: ", error)
+    }
+})();
+*/
